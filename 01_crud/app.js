300dc2a553cd3ac020cd7cbcf08ce4bd4651519b
Karla Delia <teak060525mdfrsra7@soycecytem.mx>
2022-11-09 09:12:55 -0600

    no

--- 01_crud/app.js
+++ 01_crud/app.js
@@ -10,12 +10,59 @@ tabla.innerHTML+=`
                 <td scope="row">${item.id}</td>
                 <td>${item.title}</td>
                 <td>${item.body}</td>
-                <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Sexoo"><i class="bi bi-pencil-square"></i>editar</button></td>
-                <td><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#Sexoo2"><i class="bi bi-trash"></i>eliminar</button></td>
+                <td><button type="button" class="btn btn-primary"   onclick="cargarPost(${item.id})" data-bs-toggle="modal" data-bs-target="#Sexoo"><i class="bi bi-pencil-square"></i>editar</button></td>
+                <td><button type="button" class="btn btn-danger"   onclick="eliminarPost(${item.id})" data-bs-toggle="modal" data-bs-target="#Sexoo2"><i class="bi bi-trash"></i>eliminar</button></td>
     </tr>
 
 
 `})
+}
+const cargarPost=async(id)=>{    
+    const api= await fetch('https://jsonplaceholder.typicode.com/posts/'+id);
+    const respuesta=await api.json();
+    console.log(respuesta);
+    document.querySelector("#tedit").value=respuesta.title;
+    document.querySelector("#edibody").value=respuesta.body;
+    document.querySelector("#eid").value=respuesta.id;
+    document.querySelector("#euserid").value=respuesta.userId;
+}
+const guardarPost=async()=>{    
+
+    let title=document.querySelector("#tedit").value;
+    let body=document.querySelector("#edibody").value;
+    let idd=document.querySelector("#eid").value;
+    let userid=document.querySelector("#euserid").value;
+    const post={
+        title:title,
+        body:body,
+        id:idd,
+        userId:userid
+    }
+    const api=await fetch('https://jsonplaceholder.typicode.com/posts/'+idd, {
+        method: 'PUT',
+        body: JSON.stringify(post),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    const respuesta=api.json();
+    if(respuesta!=null){
+        Swal.fire({
+            icon: 'success',
+            title: 'EDITAR',
+            text: 'SE ACTUALIZO CORRECTAMENTE!!'
+        })
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'ALGO FALLO!!!'
+        })
+    }
+
+
+
+    
 }
 
 
@@ -42,7 +89,7 @@ tr.innerHTML=`
                 <td scope="row">${respuesta.id}</td>
                 <td>${respuesta.title}</td>
                 <td>${respuesta.body}</td>
-                <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Sexoo"><i class="bi bi-pencil-square"></i>editar</button></td>
+                <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Sexoo"  ><i class="bi bi-pencil-square"></i>editar</button></td>
                 <td><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#Sexoo2"><i class="bi bi-trash"></i>eliminar</button></td>
     </tr>
 `
@@ -67,8 +114,25 @@ else{
     
 
 }
-const eliminarpost=async ()=>{
-    
-tr.innerHTML="";
-
+const eliminarPost=(id)=>{
+    document.querySelector("#deleteid").value=id;
+}
+const deletePost=async ()=>{
+    const id=document.querySelector("#deleteid").value;
+    const api=await fetch('https://jsonplaceholder.typicode.com/posts/'+id, {
+        method: 'DELETE',
+    })
+    const respuesta=api.json();
+    console.log(respuesta)
+    if(respuesta!=null){
+        Swal.fire({
+            icon: 'success',
+            title: 'ELIMINAR',
+            text: 'SE ELIMINO CORRECTAMENTE!!'
+        })
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'ERRO',
+        } )}
 }
