b940d6d02aabe89eebb0ba3281e7a004f2640ba7
Karla Delia <teak060525mdfrsra7@soycecytem.mx>
2022-11-16 10:01:58 -0600

    memoteextraño

---
+++ 06_newsid/firebase.js
@@ -0,0 +1,18 @@
+
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyD8ueD7dyw2M_wYFXCSDEpZNdpnf3CNOQM",
+    authDomain: "fir-716b4.firebaseapp.com",
+    projectId: "fir-716b4",
+    storageBucket: "fir-716b4.appspot.com",
+    messagingSenderId: "176388197575",
+    appId: "1:176388197575:web:9afae8c1ecae182bbf42e2",
+    measurementId: "G-811DL97T3T"
+  };
+
+ 
+ export const app = initializeApp(firebaseConfig);
+ 
