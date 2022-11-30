2ffe496f97f46a3f8b6e82888eac875471d939f8
Karla Delia <teak060525mdfrsra7@soycecytem.mx>
2022-11-30 09:42:17 -0600

    arriba the strokes y las tetas

--- 06_newsid/app.js
+++ 06_newsid/app.js
@@ -4,43 +4,86 @@ import { getAuth,
   createUserWithEmailAndPassword, 
   GoogleAuthProvider,
   signInWithEmailAndPassword,
-  onAuthStateChanged 
+  onAuthStateChanged,
+  signOut,
+  signInWithPopup 
 } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
-
+ 
+let user=null;
 const auth = getAuth(app);
-const provier = new GoogleAuthProvider();
-const btncrearcuenta=document.querySelector("#btncrear")
-const btngogle=document.querySelector("#btngo")
-const btnini=document.querySelector("#btniniciar")
 
 
 
+onAuthStateChanged(auth, (user) =>{
+  const containe=document.querySelector("#container");
+  checarEstado(user);
+  if(user){
+    containe.innerHTML=`<h1>BIENVENIDO: ${user.email}</h1>`
+    const uid= user.uid;
+  } else{
+    containe.innerHTML=`<h1>No hay usuarios  <i class="bi bi-person-exclamation"></i></h1>`
+  }
+})
+
+
+
+const btngogle  =document.querySelector("#btngo")
+btngogle.addEventListener('click', async(e)=>{
+e.preventDefault();
+const provider = new GoogleAuthProvider();
+try{
+  const credentials= await signInWithPopup(auth, provider)
+  user=credentials.user;
+  const modalInstance = bootstrap.Modal.getInstance(btngogle.closest('.modal'));
+  modalInstance.hide();
+  checarEstado(user)
+} catch(error){
+  console.log(error);
+}
+
+});
+
+const checarEstado=(user=null)=>{
+  console.log(user);
+  if(user==null){
+document.querySelector("#btncrear").style.display="block";
+document.querySelector("#btniniciar").style.display="block";
+document.querySelector("#cerrar").style.display="none";
+  }else{
+  document.querySelector("#btncrear").style.display="none";
+document.querySelector("#btniniciar").style.display="none";
+document.querySelector("#cerrar").style.display="block";
+  }
+}
+
+
+
+const btncr=document.querySelector("#cerrar");
+btncr.addEventListener('click', async(e)=>{
+  e.preventDefault();
+  try{
+    await signOut(auth)
+    checarEstado()
+  } catch(error){
+    console.log(error)
+  }
+});
+
+
+
+const btnini=document.querySelector("#btniniciar");
 btnini.addEventListener('click', async(e)=>{
   e.preventDefault();
   const email=document.querySelector("#iniciaremail");
   const password=document.querySelector("#iniciarcontra");
-  console.log(email.value,password.value);
-
 try{
   const res= await signInWithEmailAndPassword(auth, email.value, password.value)
-  console.log(res.user);
-  const user=res.user;
+user=res.user;
 Swal.fire('Bienvenido <i class="bi bi-person-check-fill"></i>')
 var myModalEl = document.getElementById('iniciarModal');
 var modal=bootstrap.Modal.getInstance(myModalEl)
 modal.hide();
-const res2= await onAuthStateChanged(auth, (user)=>{
-const container=document.querySelector("#container");
-if(user){
-  container.innerHTML=`<h1>${user.email} </h1>` 
-  document.querySelector("#iniciar").style.display="none";
-  document.querySelector("#crear").style.display="none";
-  const uid=user.uid;
-}
-else{
-  container.innerHTML=`<h1>No hay usuarios</h1>`
-}
-})
+
 }
 catch(error){
   Swal.fire('Usuario y o contraseña incorrecto')
@@ -48,21 +91,18 @@ catch(error){
 });
 
 
-btngogle.addEventListener('click', async(e)=>{
-
-});
-
+const btncrearcuenta=document.querySelector("#btncrear");
 btncrearcuenta.addEventListener('click', async(e)=>{
   e.preventDefault();
 const email=document.querySelector("#crearemail");
 const password=document.querySelector("#crearcontra");
-console.log(email.value,password.value);
+//console.log(email.value,password.value);
 var myModalEl=document.getElementById('crearModal');
 var modal=bootstrap.Modal.getInstance(myModalEl)
 
 try{
   const respuesta=await createUserWithEmailAndPassword (auth, email.value, password.value)
-console.log(respuesta.user);
+//console.log(respuesta.user);
 Swal.fire({
   icon: 'success',
   title: 'exito',
@@ -81,14 +121,14 @@ if (code==='auth/invalid-email'){
       text: 'correo electronico invalido',
         })
 }
-else if (code==='auth/weak-password'){
+if (code==='auth/weak-password'){
   Swal.fire({
       icon: 'error',
      
       text: 'contraseña invalida',
         })
 }
-else if (code==='auth/email-already-in-user'){
+if (code==='auth/email-already-in-user'){
   Swal.fire({
       icon: 'error',
      
@@ -96,4 +136,7 @@ else if (code==='auth/email-already-in-user'){
         })
 }
 }});
+
+
+
 
